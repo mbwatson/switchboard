a1e7dfe094d4bfaba76f1ea590911fc37c188e98
Matt Watson <mattwat@gmail.com>
2019-06-18 19:57:30 -0400

    create select and option components

--- src/components/Forms/Contact.js
+++ src/components/Forms/Contact.js
@@ -28,8 +28,40 @@ export const TextareaInput = styled.textarea`
     }
 `
 
+export const Select = styled.select`
+    width: 100%;
+    letter-spacing: 1px;
+    background-color: #fff;
+    color: var(--color-black);
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    border: 1px solid var(--color-black);
+    outline: 0;
+    transition: box-shadow 500ms, border 250ms;
+    &:focus {
+        border: 1px solid var(--color-primary);
+        box-shadow: 0 0 6px 1px rgba(var(--color-primary-shadow));
+    }
+`
+
+export const Option = styled.option`
+    padding: 1rem;
+    border: 1px solid var(--color-primary);
+`
+
+export const MessageTypeSelect = () => {
+    return (
+        <Select name="message-type-select" id="message-type-select">
+            <Option value="asd">asd</Option>
+            <Option value="qwe">qwe</Option>
+            <Option value="dfg">dfg</Option>
+        </Select>
+    )
+}
+
 export const ContactForm = props => {
     const [message, setMessage] = useState('')
+    const [type, setType] = useState()
     const [messageSent, setMessageSent] = useState(false)
     
     const handleChangeMessage = event => {
@@ -44,6 +76,7 @@ export const ContactForm = props => {
 
     return !messageSent ? (
         <FormControl>
+            <MessageTypeSelect />
             <TextareaInput placeholder="Enter message" value={ message } onChange={ handleChangeMessage } />
             <Button onClick={ handleSubmitMessage }>Submit</Button>
         </FormControl>
