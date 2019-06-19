57a570ee6ab527dda01ca51668527097e2c7207a
Matt Watson <mattwat@gmail.com>
2019-06-18 20:32:56 -0400

    reduce top and bottom menu on menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,7 +6,7 @@ export const Menu = styled.nav`
     border-right: 1px solid var(--color-grey);
     background-color: var(--color-white);
     transition: padding-top 250ms;
-    padding-top: ${ props => props.compact ? '1rem' : '3rem' };
+    padding-top: ${ props => props.compact ? '1rem' : '2rem' };
     width: ${ props => props.width }px;
     position: absolute;
     top: 0;
